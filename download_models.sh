c3fd27c7097cb646b4b0643bdfea63b7931c4d41
mrtuandao <tuan.dq1210@gmail.com>
2025-12-11 05:57:55 +0000

    update

--- download_models.sh
+++ download_models.sh
@@ -20,11 +20,10 @@ source .venv/bin/activate
 
 # # hf download MiniLLM/SFT-gpt2-120M --local-dir models/minillm_sft_gpt2-120m/
 
-wget https://huggingface.co/openai-community/gpt2/resolve/main/model.safetensors -O model_hub/gpt2/gpt2-base/model.safetensors
 
 # # BASE LINES
 # # gpt2-120m
-hf download HoangTran223/MCW_KD_GPT2_SFT-1 --local-dir MCW_KD_GPT2_SFT-1
+# hf download HoangTran223/MCW_KD_GPT2_SFT-1 --local-dir MCW_KD_GPT2_SFT-1
 # hf download HoangTran223/MCW_KD_GPT2_ULD --local-dir models/uld_gpt2-120m/
 # hf download HoangTran223/MCW_KD_GPT2_DSKD --local-dir models/dskd_gpt2-120m/
 # hf download HoangTran223/MCW_KD_GPT2_MinED --local-dir models/mined_gpt2-120m/
@@ -32,3 +31,8 @@ hf download HoangTran223/MCW_KD_GPT2_SFT-1 --local-dir MCW_KD_GPT2_SFT-1
 # hf download HoangTran223/MCW_KD_Teacher_Qwen1.5-1.8B --local-dir models/teacher_qwen1.5-1.8b/
 
 # mini
+wget https://huggingface.co/openai-community/gpt2/resolve/main/model.safetensors -O model_hub/gpt2/gpt2-base/model.safetensors
+wget https://huggingface.co/HoangTran223/MCW_KD_GPT2_SFT-1/resolve/main/pytorch_model.bin -O outputs/gpt2/gpt2-base/hoang_sft/MCW_KD_GPT2_SFT-1/pytorch_model.bin
+wget "https://huggingface.co/mrtuandao/weighted-CTKD/resolve/main/gpt2/gpt2-base/sft/criterion%3Dcross_entropy__default-bf16__epoch%3D20__bsz%3D4x2x1%3D8__lr%3D0.0005/epoch20_step28580_loss6.3126_rougel24.6371/pytorch_model.bin" -O outputs/gpt2/gpt2-base/sft/criterion=cross_entropy__default-bf16__epoch=20__bsz=4x2x1=8__lr=0.0005/epoch20_step28580_loss6.3126_rougel24.6371/pytorch_model.bin
+
+hf upload mrtuandao/weighted-CTKD outputs/ .
