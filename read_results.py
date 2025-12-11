949e99cb8ba81236deb67c607146261cc6c51bf0
tuandao-zenai <tuandq.zenai@gmail.com>
2025-12-11 12:48:16 +0700

    update

---
+++ read_results.py
@@ -0,0 +1,17 @@
+results_path = "/home/tuandao_wsl/github.com/MrTuanDao/DSKD/outputs/gpt2/gpt2-base/hoang_sft/rougeL_results.jsonl"
+
+import json
+
+results = {}
+
+with open(results_path, "r") as f:
+    for line in f.readlines():
+        result = json.loads(line)
+        dataname = result["dataname"]
+        rougeL = result["rougeL"]
+        results[dataname] = results.get(dataname, []) + [rougeL]
+
+for dataname in results:
+    dataname_results = results[dataname]
+    mean = sum(dataname_results) / len(dataname_results)
+    print(dataname, f"{mean:.2f}")
